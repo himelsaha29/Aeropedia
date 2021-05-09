8a9c8314c6ad03410446fde46092c4ce52a1b9f4
himelsaha29 <himelsaha29@live.com>
2021-05-09 01:42:31 -0400

    Complete overhaul of UI

--- app/src/main/java/com/himel/aeropedia/MainActivity.java
+++ app/src/main/java/com/himel/aeropedia/MainActivity.java
@@ -34,68 +34,7 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        bombardier = findViewById(R.id.bombardier);
-        boeing = findViewById(R.id.boeing);
-        embraer = findViewById(R.id.embraer);
-        airbus = findViewById(R.id.airbus);
 
-        Animation translate = AnimationUtils.loadAnimation(this, R.anim.animation);
-
-
-        bombardier.getBackground().setAlpha(100);
-        bombardier.setAnimation(translate);
-        airbus.getBackground().setAlpha(100);
-        airbus.setAnimation(translate);
-        boeing.getBackground().setAlpha(100);
-        boeing.setAnimation(translate);
-        embraer.getBackground().setAlpha(100);
-        embraer.setAnimation(translate);
-
-
-
-        airbus.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                // Start the loading animation when the user tap the button
-                airbus.startAnimation();
-
-                // Do your networking task or background work here.
-                final Handler handler = new Handler();
-                handler.postDelayed(new Runnable() {
-                    @Override
-                    public void run() {
-
-
-                        airbus.stopAnimation(TransitionButton.StopAnimationStyle.EXPAND, new TransitionButton.OnAnimationStopEndListener() {
-                            @Override
-                            public void onAnimationStopEnd() {
-                                Intent intent = new Intent(getBaseContext(), Airbus.class);
-                                intent.addFlags(Intent.FLAG_ACTIVITY_NO_ANIMATION);
-                                startActivity(intent);
-                            }
-                        });
-                    }
-                }, 200);
-            }
-        });
-
-
-
-        mDrawer = (FlowingDrawer) findViewById(R.id.drawerlayout);
-        mDrawer.setTouchMode(ElasticDrawer.TOUCH_MODE_FULLSCREEN);
-        mDrawer.setOnDrawerStateChangeListener(new ElasticDrawer.OnDrawerStateChangeListener() {
-            @Override
-            public void onDrawerStateChange(int oldState, int newState) {
-                if (newState == ElasticDrawer.STATE_CLOSED) {
-                    Log.d("MainActivity", "Drawer STATE_CLOSED");
-                }
-            }
-
-            @Override
-            public void onDrawerSlide(float openRatio, int offsetPixels) {
-                Log.d("MainActivity", "openRatio=" + openRatio + " ,offsetPixels=" + offsetPixels);
-            }
-        });
 
     }
 
