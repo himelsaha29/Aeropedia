3e38c76ab7b5ee330c3b44c2ebf8c3f9cb28bfca
himelsaha29 <himelsaha29@live.com>
2021-05-16 23:58:41 -0400

    FlowingDrawer added to A350 description page

--- app/src/main/java/com/himel/aeropedia/AirbusA350.java
+++ app/src/main/java/com/himel/aeropedia/AirbusA350.java
@@ -3,6 +3,11 @@ package com.himel.aeropedia;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.util.Log;
+
+import com.mxn.soul.flowingdrawer_core.ElasticDrawer;
+import com.mxn.soul.flowingdrawer_core.FlowingDrawer;
+
 
 public class AirbusA350 extends AppCompatActivity {
 
@@ -10,6 +15,23 @@ public class AirbusA350 extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_airbus_a350);
+
+        FlowingDrawer mDrawer= null;
+        mDrawer = (FlowingDrawer) findViewById(R.id.drawerlayout);
+        mDrawer.setTouchMode(ElasticDrawer.TOUCH_MODE_FULLSCREEN);
+        mDrawer.setOnDrawerStateChangeListener(new ElasticDrawer.OnDrawerStateChangeListener() {
+            @Override
+            public void onDrawerStateChange(int oldState, int newState) {
+                if (newState == ElasticDrawer.STATE_CLOSED) {
+                    Log.i("MainActivity", "Drawer STATE_CLOSED");
+                }
+            }
+
+            @Override
+            public void onDrawerSlide(float openRatio, int offsetPixels) {
+                Log.i("MainActivity", "openRatio=" + openRatio + " ,offsetPixels=" + offsetPixels);
+            }
+        });
     }
 
 
