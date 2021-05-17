68ca5ade8688d5a38b802a13d4849c6494cd1af8
himelsaha29 <himelsaha29@live.com>
2021-05-17 14:22:44 -0400

    Language toggles between English and French

--- app/src/main/java/com/himel/aeropedia/AirbusA350.java
+++ app/src/main/java/com/himel/aeropedia/AirbusA350.java
@@ -2,22 +2,49 @@ package com.himel.aeropedia;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.app.Activity;
+import android.content.Intent;
+import android.content.SharedPreferences;
+import android.content.res.Configuration;
 import android.graphics.Color;
 import android.os.Bundle;
 import android.util.Log;
+import android.view.View;
+import android.widget.Button;
 
 import com.mxn.soul.flowingdrawer_core.ElasticDrawer;
 import com.mxn.soul.flowingdrawer_core.FlowingDrawer;
 
-
-
+import java.util.Locale;
 
 public class AirbusA350 extends AppCompatActivity {
+
+    private Button langToggle;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_airbus_a350);
+        langToggle = findViewById(R.id.lang_toggle);
+
+
+        langToggle.setOnClickListener(new View.OnClickListener() {
+
+            @Override
+            public void onClick(View v) {
+                System.out.println(getResources().getConfiguration().locale);
+                System.out.println(Locale.FRANCE);
+                if(getResources().getConfiguration().locale.toString().contains("fr")) {
+                    setLocale("en");
+                    recreate();
+                } else if (getResources().getConfiguration().toString().contains("en")) {
+                    setLocale("fr");
+                    recreate();
+                }
+            }
+        });
+
+
 
         FlowingDrawer mDrawer= null;
         mDrawer = (FlowingDrawer) findViewById(R.id.drawerlayout);
@@ -37,6 +64,24 @@ public class AirbusA350 extends AppCompatActivity {
         });
     }
 
+
+
+    private void setLocale(String language) {
+        Locale locale = new Locale(language);
+        Locale.setDefault(locale);
+        Configuration config = new Configuration();
+        config.locale = locale;
+        getBaseContext().getResources().updateConfiguration(config, getBaseContext().getResources().getDisplayMetrics());
+        SharedPreferences.Editor editor = getSharedPreferences("Settings", MODE_PRIVATE).edit();
+        editor.putString("Language", language);
+        editor.apply();
+    }
+
+    private void loadLocale() {
+        SharedPreferences prefs = getSharedPreferences("Settings", Activity.MODE_PRIVATE);
+        String language = prefs.getString("Language", "");
+        setLocale(language);
+    }
 
     @Override
     public void finish() {
