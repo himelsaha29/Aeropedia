2f990a970e3a08fe80a42ed4b94c77c2135ea219
himelsaha29 <himelsaha29@live.com>
2021-06-06 14:33:30 -0400

    Drawer added to Alexa

--- app/src/main/java/com/himel/aeropedia/alexa/AlexaActivity.java
+++ app/src/main/java/com/himel/aeropedia/alexa/AlexaActivity.java
@@ -12,6 +12,7 @@ import androidx.annotation.Nullable;
 import androidx.core.app.ActivityCompat;
 import androidx.core.content.ContextCompat;
 
+import android.util.Log;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.TextView;
@@ -19,6 +20,11 @@ import android.widget.TextView;
 import com.airbnb.lottie.LottieAnimationView;
 import com.himel.aeropedia.R;
 import com.himel.aeropedia.airbus.AirbusA350;
+import com.himel.aeropedia.treeview.IconTreeItemHolder;
+import com.mxn.soul.flowingdrawer_core.ElasticDrawer;
+import com.mxn.soul.flowingdrawer_core.FlowingDrawer;
+import com.unnamed.b.atv.model.TreeNode;
+import com.unnamed.b.atv.view.AndroidTreeView;
 import com.willblaschko.android.alexa.AlexaManager;
 import com.willblaschko.android.alexa.AuthorizationManager;
 import com.willblaschko.android.alexa.callbacks.AsyncCallback;
@@ -50,6 +56,9 @@ public class AlexaActivity extends CoreActivity {
     private NeumorphButton login;
     private boolean loggedIn = false;
     private boolean speak = false;
+    private FlowingDrawer mDrawer;
+    private BlurLayout blur;
+    private AndroidTreeView tView;
 
 
     @Override
@@ -68,12 +77,14 @@ public class AlexaActivity extends CoreActivity {
                     alexaManager.sendAudioRequest(requestBody, getRequestCallback());
                 }
             });
+            loadDrawer();
         }
         else {
             loadAlexa();
-
-
+            loadDrawer();
         }
+
+
 
     }
 
@@ -128,6 +139,35 @@ public class AlexaActivity extends CoreActivity {
                 dialog.show();
             }
         });
+    }
+
+    private void loadDrawer() {
+        mDrawer = findViewById(R.id.drawerlayout);
+        blur = findViewById(R.id.blurLayout);
+
+        mDrawer.setTouchMode(ElasticDrawer.TOUCH_MODE_FULLSCREEN);
+        mDrawer.setOnDrawerStateChangeListener(new ElasticDrawer.OnDrawerStateChangeListener() {
+            @Override
+            public void onDrawerStateChange(int oldState, int newState) {
+                if (newState == ElasticDrawer.STATE_CLOSING) {
+                    Log.i("MainActivity", "Drawer STATE_CLOSED");
+                    blur.setVisibility(View.GONE);
+                    blur.setAlpha(0f);
+                } else if (newState == ElasticDrawer.STATE_OPENING) {
+                    blur.invalidate();
+                    blur.setAlpha(0.0f);
+                    blur.setVisibility(View.VISIBLE);
+                    blur.animate().alpha(1.0f).setDuration(1400);
+                }
+            }
+
+            @Override
+            public void onDrawerSlide(float openRatio, int offsetPixels) {
+                Log.i("MainActivity", "openRatio=" + openRatio + " ,offsetPixels=" + offsetPixels);
+            }
+        });
+
+        createTreeView();
     }
 
     @Override
@@ -276,6 +316,7 @@ public class AlexaActivity extends CoreActivity {
 
             loggedIn = true;
             loadAlexa();
+            loadDrawer();
 
             if (ContextCompat.checkSelfPermission(this,
                     Manifest.permission.RECORD_AUDIO)
@@ -326,5 +367,72 @@ public class AlexaActivity extends CoreActivity {
         return loggedIn[0];
 
     }
+
+
+    /** TreeView **/
+
+    private void createTreeView() {
+
+        ViewGroup containerView = (ViewGroup) findViewById(R.id.inside);
+
+
+        TreeNode root = TreeNode.root();
+        TreeNode manufacturerRoot = new TreeNode(new IconTreeItemHolder.IconTreeItem(R.string.ic_laptop, "Manufacturers", "No", "Manufacturers"));
+        TreeNode amazonRoot = new TreeNode(new IconTreeItemHolder.IconTreeItem(R.drawable.ic_amazon_alexa, "Amazon Alexa", "No", "Alexa"));
+        TreeNode firebaseRoot = new TreeNode(new IconTreeItemHolder.IconTreeItem(R.string.ic_laptop, "Firebase", "No", "firebase"));
+
+
+
+        TreeNode airbus = null;
+        //if (verifyDarkMode().equals("Yes")) {
+            airbus = new TreeNode(new IconTreeItemHolder.IconTreeItem(R.string.ic_folder, this.getString(R.string.airbus), "HighlightLight", "airbus"));
+//        } else {
+//            airbus = new TreeNode(new IconTreeItemHolder.IconTreeItem(R.string.ic_folder, this.getString(R.string.airbus), "HighlightDark", "airbus"));
+//        }
+        TreeNode a220Node = new TreeNode(new IconTreeItemHolder.IconTreeItem(R.string.ic_drive_file, this.getString(R.string.a220), "No", "a220"));
+        TreeNode a319Node = new TreeNode(new IconTreeItemHolder.IconTreeItem(R.string.ic_drive_file, this.getString(R.string.a319), "No", "a319"));
+        TreeNode a320Node = new TreeNode(new IconTreeItemHolder.IconTreeItem(R.string.ic_drive_file, this.getString(R.string.a320), "No", "a320"));
+        TreeNode a321Node = new TreeNode(new IconTreeItemHolder.IconTreeItem(R.string.ic_drive_file, this.getString(R.string.a321), "No", "a321"));
+
+        TreeNode a330Node = new TreeNode(new IconTreeItemHolder.IconTreeItem(R.string.ic_alexa, this.getString(R.string.a330), "No", "a330"));
+        TreeNode a340Node = new TreeNode(new IconTreeItemHolder.IconTreeItem(R.string.ic_drive_file, this.getString(R.string.a340), "No", "a340"));
+        TreeNode a350Node = new TreeNode(new IconTreeItemHolder.IconTreeItem(R.string.ic_drive_file, this.getString(R.string.a350), "No", "a350"));
+        TreeNode a380Node = new TreeNode(new IconTreeItemHolder.IconTreeItem(R.string.ic_drive_file, this.getString(R.string.a380), "No", "a380"));
+
+
+        airbus.addChildren(a220Node, a319Node, a320Node, a321Node, a330Node, a340Node, a350Node, a380Node);
+
+
+        TreeNode boeing = new TreeNode(new IconTreeItemHolder.IconTreeItem(R.string.ic_photo_library, this.getString(R.string.boeing), "No", "boeing"));
+        TreeNode b777 = new TreeNode(new IconTreeItemHolder.IconTreeItem(R.string.ic_photo, "B777", "No", "b777"));
+        TreeNode b787 = new TreeNode(new IconTreeItemHolder.IconTreeItem(R.string.ic_photo, "B787", "No", "b787"));
+        boeing.addChildren(b777, b787);
+
+        manufacturerRoot.addChildren(airbus, boeing);
+
+
+        root.addChildren(manufacturerRoot);
+        root.addChildren(amazonRoot);
+        root.addChildren(firebaseRoot);
+        manufacturerRoot.setExpanded(true);
+        airbus.setExpanded(true);
+
+
+        tView = new AndroidTreeView(this, root);
+        tView.setDefaultContainerStyle(R.style.TreeNodeStyleCustom);
+        tView.setDefaultViewHolder(IconTreeItemHolder.class);
+
+        containerView.addView(tView.getView());
+
+//        if (savedInstanceState != null) {
+//            String state = savedInstanceState.getString("tState");
+//            if (!TextUtils.isEmpty(state)) {
+//                tView.restoreState(state);
+//            }
+//        }
+
+    }
+
+    /** TreeView **/
 
 }
