7c555fa0f491827a359517e3cad5cdb824229cb0
himelsaha29 <himelsaha29@live.com>
2021-06-11 12:30:35 -0400

    Transitioning from OpenSky Java API to REST API

--- app/src/main/java/com/himel/aeropedia/mapbox/FlightMap.java
+++ app/src/main/java/com/himel/aeropedia/mapbox/FlightMap.java
@@ -32,14 +32,25 @@ import java.io.IOException;
 import java.sql.SQLOutput;
 import java.util.ArrayList;
 import java.util.List;
+import java.util.concurrent.TimeUnit;
 
 import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.core.content.res.ResourcesCompat;
 
+import org.jetbrains.annotations.NotNull;
+import org.json.JSONArray;
+import org.json.JSONObject;
+import org.json.JSONException;
 import org.opensky.api.OpenSkyApi;
 import org.opensky.model.OpenSkyStates;
 import org.opensky.model.StateVector;
+
+import okhttp3.Call;
+import okhttp3.Callback;
+import okhttp3.OkHttpClient;
+import okhttp3.Request;
+import okhttp3.Response;
 
 import static com.mapbox.mapboxsdk.style.expressions.Expression.get;
 import static com.mapbox.mapboxsdk.style.expressions.Expression.literal;
@@ -158,7 +169,7 @@ public class FlightMap extends AppCompatActivity implements
             } catch (Exception e) {
                 System.out.println("CAUGHT : " + e.getMessage());
             }
-            System.out.println("PRINTS = " + x);
+            //System.out.println("PRINTS = " + x);
             symbolLayerIconFeatureList.add(Feature.fromGeometry(Point.fromLngLat(longitude, latitude)));
         }
 
@@ -226,7 +237,7 @@ public class FlightMap extends AppCompatActivity implements
                     } catch (IOException e) {
                         System.out.println(e);
                     }
-                    //System.out.println("Number of states: " + states.getStates().size());
+                    System.out.println("Number of states: " + os.getStates().size());
                     System.out.println(os.getStates().size());
                     for(StateVector s : os.getStates()) {
                         sv.add(s);
@@ -236,6 +247,43 @@ public class FlightMap extends AppCompatActivity implements
                     e.printStackTrace();
                     System.out.println("EXCEPTION");
                 }
+
+
+                OkHttpClient.Builder builder = new OkHttpClient.Builder();
+                builder.connectTimeout(30, TimeUnit.SECONDS);
+                builder.readTimeout(30, TimeUnit.SECONDS);
+                builder.writeTimeout(30, TimeUnit.SECONDS);
+                OkHttpClient client = builder.build();
+
+                String url = BASE_URL + "/states/all";
+                Request request = new Request.Builder()
+                        .url(url)
+                        .build();
+                client.newCall(request).enqueue(new Callback() {
+                    @Override
+                    public void onFailure(@NotNull Call call, @NotNull IOException e) {
+                        e.printStackTrace();
+                        System.out.println("OKHTTP : EXCEPTION");
+                    }
+
+                    @Override
+                    public void onResponse(@NotNull Call call, @NotNull Response response) throws IOException {
+                        if(response.isSuccessful()) {
+                            String responseData = response.body().string();
+                            try {
+                                JSONObject jsonObject = new JSONObject(responseData);
+                                System.out.println(jsonObject);
+                                JSONArray jsonArray = jsonObject.getJSONArray("states");
+                                System.out.println(jsonArray.length());
+                                System.out.println("jsonARRAY CONTENT : " + jsonArray.toString());
+                            } catch (JSONException e) {
+                                System.out.println("JSONARRAY EXCEPTION: === " + e.getMessage());
+                                e.printStackTrace();
+                            }
+                            System.out.println(" RESPONSE : === " + response.body());
+                        }
+                    }
+                });
             }
 
         });
