87ca459befdcbbdbeaf936a3c1da3bf3b720c980
himelsaha29 <himelsaha29@live.com>
2021-07-06 23:57:04 -0400

    A350 slide photos added

--- app/src/main/java/com/himel/aeropedia/airbus/AirbusA350.java
+++ app/src/main/java/com/himel/aeropedia/airbus/AirbusA350.java
@@ -334,7 +334,7 @@ public class AirbusA350 extends AppCompatActivity {
 
         adapter = new SliderAdapter(this);
         sliderView.setSliderAdapter(adapter, false);
-        sliderView.setIndicatorAnimation(IndicatorAnimationType.WORM); //set indicator animation by using SliderLayout.IndicatorAnimations. :WORM or THIN_WORM or COLOR or DROP or FILL or NONE or SCALE or SCALE_DOWN or SLIDE and SWAP!!
+        sliderView.setIndicatorAnimation(IndicatorAnimationType.WORM);
         sliderView.setSliderTransformAnimation(SliderAnimations.SIMPLETRANSFORMATION);
         sliderView.setAutoCycleDirection(SliderView.AUTO_CYCLE_DIRECTION_BACK_AND_FORTH);
         sliderView.setIndicatorSelectedColor(Color.WHITE);
