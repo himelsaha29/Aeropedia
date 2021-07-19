152f6671b3eae935655868cca8d18893e22008cd
himelsaha29 <himelsaha29@live.com>
2021-07-19 00:44:38 -0400

    A300 done

--- app/src/main/java/com/himel/aeropedia/airbus/AirbusA310.java
+++ app/src/main/java/com/himel/aeropedia/airbus/AirbusA310.java
@@ -349,13 +349,13 @@ public class AirbusA310 extends AppCompatActivity {
         for (int i = 0; i < 4; i++) {
             SliderItem sliderItem = new SliderItem();
             if (i == 0) {
-                sliderItem.setImageLocation(R.drawable.a310_slider1);
+                sliderItem.setImageLocation(R.drawable.a310_slider01);
             } else if (i == 1) {
-                sliderItem.setImageLocation(R.drawable.a310_slider2);
+                sliderItem.setImageLocation(R.drawable.a310_slider02);
             } else if (i == 2) {
-                sliderItem.setImageLocation(R.drawable.a310_slider3);
+                sliderItem.setImageLocation(R.drawable.a310_slider03);
             } else if (i == 3) {
-                sliderItem.setImageLocation(R.drawable.a310_slider4);
+                sliderItem.setImageLocation(R.drawable.a310_slider04);
             }
 
             sliderItemList.add(sliderItem);
