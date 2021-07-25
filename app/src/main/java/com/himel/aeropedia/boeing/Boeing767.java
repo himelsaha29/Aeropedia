32e20a60c3e3c92c92cd80248971ae891ff9f9a3
himelsaha29 <himelsaha29@live.com>
2021-07-25 02:05:52 -0400

    Boeing 767 photos added

--- app/src/main/java/com/himel/aeropedia/boeing/Boeing767.java
+++ app/src/main/java/com/himel/aeropedia/boeing/Boeing767.java
@@ -379,13 +379,13 @@ public class Boeing767 extends AppCompatActivity {
         for (int i = 0; i < 4; i++) {
             SliderItem sliderItem = new SliderItem();
             if (i == 0) {
-                sliderItem.setImageLocation(R.drawable.boeing_767_slider1);
+                sliderItem.setImageLocation(R.drawable.boeing_767_slider01);
             } else if (i == 1) {
-                sliderItem.setImageLocation(R.drawable.boeing_767_slider2);
+                sliderItem.setImageLocation(R.drawable.boeing_767_slider02);
             } else if (i == 2) {
-                sliderItem.setImageLocation(R.drawable.boeing_767_slider3);
+                sliderItem.setImageLocation(R.drawable.boeing_767_slider03);
             } else if (i == 3) {
-                sliderItem.setImageLocation(R.drawable.boeing_767_slider4);
+                sliderItem.setImageLocation(R.drawable.boeing_767_slider04);
             }
 
             sliderItemList.add(sliderItem);
