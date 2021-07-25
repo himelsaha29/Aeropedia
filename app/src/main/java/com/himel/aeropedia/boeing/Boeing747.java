23142e6fe925b392ae33d7cf10b9b41749a798a7
himelsaha29 <himelsaha29@live.com>
2021-07-25 02:05:01 -0400

    Boeing 747 photos added

--- app/src/main/java/com/himel/aeropedia/boeing/Boeing747.java
+++ app/src/main/java/com/himel/aeropedia/boeing/Boeing747.java
@@ -379,13 +379,13 @@ public class Boeing747 extends AppCompatActivity {
         for (int i = 0; i < 4; i++) {
             SliderItem sliderItem = new SliderItem();
             if (i == 0) {
-                sliderItem.setImageLocation(R.drawable.boeing_747_slider1);
+                sliderItem.setImageLocation(R.drawable.boeing_747_slider01);
             } else if (i == 1) {
-                sliderItem.setImageLocation(R.drawable.boeing_747_slider2);
+                sliderItem.setImageLocation(R.drawable.boeing_747_slider02);
             } else if (i == 2) {
-                sliderItem.setImageLocation(R.drawable.boeing_747_slider3);
+                sliderItem.setImageLocation(R.drawable.boeing_747_slider03);
             } else if (i == 3) {
-                sliderItem.setImageLocation(R.drawable.boeing_747_slider4);
+                sliderItem.setImageLocation(R.drawable.boeing_747_slider04);
             }
 
             sliderItemList.add(sliderItem);
