cb76abb86178b2308937401bdef77d843c635852
himelsaha29 <himelsaha29@live.com>
2021-07-25 22:00:49 -0400

    Boeing 787 dark added

--- app/src/main/java/com/himel/aeropedia/boeing/Boeing787.java
+++ app/src/main/java/com/himel/aeropedia/boeing/Boeing787.java
@@ -78,9 +78,9 @@ public class Boeing787 extends AppCompatActivity {
         locale = Locale.getDefault();
         enableDarkOnCreate = verifyDarkMode();
         if(enableDark.equals("No")) {
-            setContentView(R.layout.activity_boeing_787);
+            setContentView(R.layout.activity_boeing_787_light);
         } else {
-            setContentView(R.layout.activity_boeing_777_dark);
+            setContentView(R.layout.activity_boeing_787_dark);
         }
         langToggle = findViewById(R.id.lang_toggle);
         darkToggle = findViewById(R.id.dark_toggle);
