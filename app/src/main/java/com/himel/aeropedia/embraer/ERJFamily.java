f23d300b4321f39e5173a0222aefd23b00d881d8
himelsaha29 <himelsaha29@live.com>
2021-07-28 12:19:12 -0400

    Embraer ERJ Family initiated

---
+++ app/src/main/java/com/himel/aeropedia/embraer/ERJFamily.java
@@ -0,0 +1,16 @@
+package com.himel.aeropedia.embraer;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.himel.aeropedia.R;
+
+public class ERJFamily extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_erj_family_light);
+    }
+}
