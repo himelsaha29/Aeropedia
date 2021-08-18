a26942389fe52b2efa4296baba9e9e080e4803a6
himelsaha29 <himelsaha29@live.com>
2021-08-17 23:54:41 -0400

    Challenger 650 translation added

--- app/src/main/java/com/himel/aeropedia/home/SplashScreen.java
+++ app/src/main/java/com/himel/aeropedia/home/SplashScreen.java
@@ -3,7 +3,10 @@ package com.himel.aeropedia.home;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
+import android.graphics.drawable.Drawable;
+import android.graphics.drawable.TransitionDrawable;
 import android.os.Bundle;
+import android.widget.ImageView;
 
 import com.himel.aeropedia.R;
 
@@ -12,11 +15,12 @@ import java.util.TimerTask;
 
 
 public class SplashScreen extends AppCompatActivity {
-
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_splash_screen);
+
+
         Timer timer = new Timer();
         timer.schedule(new TimerTask(){
 
@@ -24,7 +28,7 @@ public class SplashScreen extends AppCompatActivity {
             public void run() {
                 Intent home = new Intent(SplashScreen.this, MainActivity.class);
                 startActivity(home);
-                overridePendingTransition(R.anim.zoom_out, R.anim.zoom_out);
+                overridePendingTransition(R.anim.zoom_in, R.anim.zoom_in);
                 finish();
             }}, 2300);
     }
