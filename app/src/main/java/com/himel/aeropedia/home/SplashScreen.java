4d4665ae963eca67fef209a305a03f4cee83011c
himelsaha29 <himelsaha29@live.com>
2021-08-18 15:34:01 -0400

    SplashScreen loaded from theme instead of loading layout

--- app/src/main/java/com/himel/aeropedia/home/SplashScreen.java
+++ app/src/main/java/com/himel/aeropedia/home/SplashScreen.java
@@ -19,31 +19,19 @@ import java.util.TimerTask;
 
 
 public class SplashScreen extends AppCompatActivity {
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_splash_screen);
-
-
-        View decorView = getWindow().getDecorView();
-        int uiOptions = View.SYSTEM_UI_FLAG_HIDE_NAVIGATION
-                | View.SYSTEM_UI_FLAG_FULLSCREEN
-                | View.SYSTEM_UI_FLAG_LOW_PROFILE
-                | View.SYSTEM_UI_FLAG_LAYOUT_STABLE
-                | View.SYSTEM_UI_FLAG_IMMERSIVE_STICKY
-                | View.SYSTEM_UI_FLAG_LAYOUT_HIDE_NAVIGATION;
-        decorView.setSystemUiVisibility(uiOptions);
-
 
         Timer timer = new Timer();
         timer.schedule(new TimerTask(){
 
             @Override
             public void run() {
-                Intent home = new Intent(SplashScreen.this, MainActivity.class);
-                startActivity(home);
+                startActivity(new Intent(SplashScreen.this, MainActivity.class));
                 overridePendingTransition(R.anim.zoom_in, R.anim.zoom_in);
                 finish();
-            }}, 2300);
+            }}, 1500);
     }
 }
