fbb0791a710450a08b40c8d36913c43af2080281
himelsaha29 <himelsaha29@live.com>
2021-08-17 20:56:04 -0400

    SplashScreen to MainActivity transitions with zoom out

--- app/src/main/java/com/himel/aeropedia/home/SplashScreen.java
+++ app/src/main/java/com/himel/aeropedia/home/SplashScreen.java
@@ -2,9 +2,14 @@ package com.himel.aeropedia.home;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
 
 import com.himel.aeropedia.R;
+
+import java.util.Timer;
+import java.util.TimerTask;
+
 
 public class SplashScreen extends AppCompatActivity {
 
@@ -12,5 +17,15 @@ public class SplashScreen extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_splash_screen);
+        Timer timer = new Timer();
+        timer.schedule(new TimerTask(){
+
+            @Override
+            public void run() {
+                Intent home = new Intent(SplashScreen.this, MainActivity.class);
+                startActivity(home);
+                overridePendingTransition(R.anim.zoom_out, R.anim.zoom_out);
+                finish();
+            }}, 2300);
     }
 }
