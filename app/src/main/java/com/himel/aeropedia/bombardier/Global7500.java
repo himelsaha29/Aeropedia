c50e1a0d082f16eac2829b343bd12abc596ed6ee
himelsaha29 <himelsaha29@live.com>
2021-08-18 10:43:16 -0400

    Global 7500 specs header fixed

--- app/src/main/java/com/himel/aeropedia/bombardier/Global7500.java
+++ app/src/main/java/com/himel/aeropedia/bombardier/Global7500.java
@@ -3,15 +3,20 @@ package com.himel.aeropedia.bombardier;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.app.Activity;
+import android.content.Context;
 import android.content.Intent;
 import android.content.SharedPreferences;
 import android.content.res.Configuration;
 import android.graphics.Color;
 import android.graphics.Typeface;
+import android.os.Build;
 import android.os.Bundle;
 import android.util.Log;
+import android.view.Display;
 import android.view.View;
 import android.view.ViewGroup;
+import android.view.Window;
+import android.view.WindowManager;
 
 import com.google.android.material.appbar.CollapsingToolbarLayout;
 import com.himel.aeropedia.R;
@@ -98,6 +103,12 @@ public class Global7500 extends AppCompatActivity {
         } else {
             setContentView(R.layout.activity_bombardier_global_7500_dark);
         }
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.LOLLIPOP) {
+            Window window = getWindow();
+            window.clearFlags(WindowManager.LayoutParams.FLAG_LAYOUT_NO_LIMITS);
+            window.addFlags(WindowManager.LayoutParams.FLAG_DRAWS_SYSTEM_BAR_BACKGROUNDS);
+            window.setStatusBarColor(Color.parseColor("#ff80aad4"));
+        }
         langToggle = findViewById(R.id.lang_toggle);
         darkToggle = findViewById(R.id.dark_toggle);
         mDrawer = findViewById(R.id.drawerlayout);
@@ -157,6 +168,8 @@ public class Global7500 extends AppCompatActivity {
             }
         });
 
+        Display display = ((android.view.WindowManager)getSystemService(Context.WINDOW_SERVICE)).getDefaultDisplay();
+        mDrawer.setMenuSize((int)(display.getWidth()*0.635));
 
         mDrawer.setTouchMode(ElasticDrawer.TOUCH_MODE_BEZEL);
         mDrawer.setOnDrawerStateChangeListener(new ElasticDrawer.OnDrawerStateChangeListener() {
@@ -412,14 +425,6 @@ public class Global7500 extends AppCompatActivity {
         sliderView.setScrollTimeInSec(3);
         sliderView.setAutoCycle(false);
         renewItems(sliderView);
-
-
-        sliderView.setOnIndicatorClickListener(new DrawController.ClickListener() {
-            @Override
-            public void onIndicatorClicked(int position) {
-                Log.i("GGG", "onIndicatorClicked: " + sliderView.getCurrentPagePosition());
-            }
-        });
     }
 
     private void renewItems(View view) {
@@ -440,18 +445,6 @@ public class Global7500 extends AppCompatActivity {
         }
         adapter.renewItems(sliderItemList);
     }
-
-    private void removeLastItem(View view) {
-        if (adapter.getCount() - 1 >= 0)
-            adapter.deleteItem(adapter.getCount() - 1);
-    }
-
-    private void addNewItem(View view) {
-        SliderItem sliderItem = new SliderItem();
-        //sliderItem.setImageUrl("https://images.pexels.com/photos/929778/pexels-photo-929778.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=750&w=1260");
-        adapter.addItem(sliderItem);
-    }
-
     /** SlideView **/
 
 }
