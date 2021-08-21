5603cd49c5da53e622afda50b6b7cd9c07b7451a
himelsaha29 <himelsaha29@live.com>
2021-08-20 23:48:42 -0400

    Citation Longitude specs header fixed

--- app/src/main/java/com/himel/aeropedia/cessna/CitationLongitude.java
+++ app/src/main/java/com/himel/aeropedia/cessna/CitationLongitude.java
@@ -106,7 +106,7 @@ public class CitationLongitude extends AppCompatActivity {
             Window window = getWindow();
             window.clearFlags(WindowManager.LayoutParams.FLAG_LAYOUT_NO_LIMITS);
             window.addFlags(WindowManager.LayoutParams.FLAG_DRAWS_SYSTEM_BAR_BACKGROUNDS);
-            window.setStatusBarColor(Color.parseColor("#ff3f78bb"));
+            window.setStatusBarColor(Color.parseColor("#ff4079bc"));
         }
         langToggle = findViewById(R.id.lang_toggle);
         darkToggle = findViewById(R.id.dark_toggle);
