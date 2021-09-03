38b20406d267afb7e563ec47338097f04605e403
himelsaha29 <himelsaha29@live.com>
2021-09-03 16:20:34 -0400

    Notification shade color modified

--- app/src/main/java/com/himel/aeropedia/airbus/AirbusA300.java
+++ app/src/main/java/com/himel/aeropedia/airbus/AirbusA300.java
@@ -106,7 +106,7 @@ public class AirbusA300 extends AppCompatActivity {
             Window window = getWindow();
             window.clearFlags(WindowManager.LayoutParams.FLAG_LAYOUT_NO_LIMITS);
             window.addFlags(WindowManager.LayoutParams.FLAG_DRAWS_SYSTEM_BAR_BACKGROUNDS);
-            window.setStatusBarColor(Color.parseColor("#ffaedcfd"));
+            window.setStatusBarColor(Color.parseColor("#ff5182c4"));
         }
         langToggle = findViewById(R.id.lang_toggle);
         darkToggle = findViewById(R.id.dark_toggle);
